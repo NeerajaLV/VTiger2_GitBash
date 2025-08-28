e7a609a97634ad2da2a017efe5257dd98c43157b
NeerajaLV <neerajalingareddy499@gmail.com>
2025-08-28 18:10:22 +0530

    commit3

---
+++ src/test/java/practice/amazon/producttest/Sample2.java
@@ -0,0 +1,17 @@
+package practice.amazon.producttest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import com.vtiger.crm.generic.webdriverutility.WebdriverUtility;
+
+public class Sample2 {
+	@Test
+	public void sample()
+	{
+		System.out.println("Pull practice");
+
+	}
+}
